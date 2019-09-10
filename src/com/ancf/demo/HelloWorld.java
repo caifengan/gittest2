0a5733947586df8f4172f223f123f4c94519c3f9
caifengan <ancaifeng123@126.com>
2019-09-10 14:48:46 +0800

    test4

--- src/com/ancf/demo/HelloWorld.java
+++ src/com/ancf/demo/HelloWorld.java
@@ -3,8 +3,8 @@ package com.ancf.demo;
 /**
  * Created by Administrator on 2019/9/10.
  */
-public class Test1 {
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("qwe1");
+        System.out.println("HelloWorld");
     }
 }
